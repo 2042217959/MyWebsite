84cea7970efa295231d664a781b79a758370470b
wangzimian <2042217959@qq.com>
2024-12-25 23:36:41 +0800

    feat 给首页的关于我添加了跳转功能

--- src/view/Home/index.jsx
+++ src/view/Home/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { HomeWrapper, Hero, About, Skills } from './style'
+import { useNavigate } from 'react-router-dom'
+import { motion } from 'framer-motion'
+import { useTheme } from '../../context/ThemeContext'
 import { 
   FaHtml5, 
   FaJs, 
@@ -9,6 +12,8 @@ import {
 import { SiWebpack } from 'react-icons/si'
 
 const Home = () => {
+  const navigate = useNavigate()
+  const { theme } = useTheme()
   const skills = [
     { icon: <FaHtml5 />, name: 'HTML/CSS' },
     { icon: <FaJs />, name: 'JavaScript' },
@@ -25,8 +30,23 @@ const Home = () => {
       </Hero>
 
       <About>
-        <h2>关于我</h2>
-        <p>这里是一段简短的自我介绍...</p>
+        <motion.h2
+          initial={{ color: theme.text.primary }}
+          animate={{ color: theme.text.primary }}
+          whileHover={{ 
+            color: theme.primary,
+            transition: { duration: 0.2 }
+          }}
+          onClick={() => navigate('/about')}
+          style={{ 
+            cursor: 'pointer',
+            display: 'inline-block'
+          }}
+          title="点击查看更多关于我的信息哦~"
+        >
+          关于我
+        </motion.h2>
+        <p>这里是魔芋小狗，我是一名前端工程师</p>
       </About>
 
       <Skills>
